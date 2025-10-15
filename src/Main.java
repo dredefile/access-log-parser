26da1cc64d8ce6679b84c3ca6887360df25c8e55
Kostomarov Nikita <dredefiles@gmail.com>
2025-10-15 12:24:19 +0300

    Базовые типы данных: прописана логика сложения, вычитания, умножения и деления для типов int и double

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int diff = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double division = (double) firstNumber / secondNumber;
+        System.out.println("Сумма = "+ sum);
+        System.out.println("Разность = " + diff);
+        System.out.println("Произведение = " + multiplication);
+        System.out.println("Частное = " + division);
     }
 }
