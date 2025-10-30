067ba18b9742f5a648a60eeb4af93d39f956b3f0
Kostomarov Nikita <dredefiles@gmail.com>
2025-10-31 00:14:17 +0300

    Создана логика ввода пути файла через консоль и валидация данного пути

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 0;
+
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists) {
+                System.out.println("Файл не существует. Попробуйте снова.");
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Указанный путь ведёт к папке, а не к файлу. Попробуйте снова.");
+                continue;
+            }
+            count++;
+            System.out.println("Путь указан верно!");
+            System.out.println("Это файл номер " + count);
+        }
     }
 }
